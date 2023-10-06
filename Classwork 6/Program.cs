33c9d723e47f8d78a7b65c9d593623703b2ac994
Karim <karimissanovts@gmail.com>
2023-10-07 02:57:21 +0600

    Homework 6

---
+++ Classwork 6/Program.cs
@@ -0,0 +1,162 @@
+﻿// CONST
+int[] CreateArray(int min, int max, int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+// -------------------------------------------------------
+
+
+// int[] ReverseArray(int[] array)
+// {
+//     int sizeArr = array.Length;
+//     for (int i = 0; i < sizeArr / 2; i++)
+//     {
+//         int temp = array[i];
+//         array[i] = array[sizeArr - 1 - i];
+//         array[sizeArr - 1 - i] = temp;
+//     }
+//     return array;
+// }
+// Console.Write("Enter min of array: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter max of array: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+
+// int[] newArray = CreateArray(min, max, size);
+// ShowArray(newArray);
+// Console.WriteLine();
+// ShowArray(ReverseArray(newArray));
+
+// -----------------------------------------------------------
+
+
+// bool IsTriangle(int a, int b, int c)
+// {
+//     return a + b > c && a + c > b && b + c > a;
+// }
+
+
+
+// Console.Write("Input first part: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second part: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input third part: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+
+// if (IsTriangle(a, b, c))
+// {
+//     Console.WriteLine("triangle can be alive");
+// }
+// else
+// {
+//     Console.WriteLine("You died!");
+// }
+
+
+// -------------------------------------------------------------
+
+
+// int BinaryNumber(int Decimal)
+// {
+//     int result = 0;
+//     int power = 0;
+//     while (Decimal > 0)
+//     {
+//         result += Decimal % 2 * Convert.ToInt32(Math.Pow(10, power));
+//         power++;
+//         Decimal /= 2;
+//     }
+//     return result;
+// }
+// ---------------------------------------------------------------------
+
+
+// void PrintFibonachi(int n, int firstnumfib, int secondnumfib)
+// {
+//     int[] fibnum = new int[n];
+//     fibnum[0] = firstnumfib;
+//     fibnum[1] = secondnumfib;
+//     Console.Write(fibnum[0] + " ");
+//     Console.Write(fibnum[1] + " ");
+//     for (int i = 2; i < n; i++)
+//     {
+//         fibnum[i] = fibnum[i - 1] + fibnum[i - 2];
+//         Console.Write(fibnum[i] + " ");
+//     }
+// }
+
+// Console.Write("Enter n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter first num: ");
+// int firstnumfib = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter second num: ");
+// int secondnumfib = Convert.ToInt32(Console.ReadLine());
+
+
+// if (n >= 0 && firstnumfib < secondnumfib)
+// {
+//     PrintFibonachi(n, firstnumfib, secondnumfib);
+// }
+// else
+// {
+//     Console.WriteLine("Error!");
+// }
+
+// -----------------------------------------
+
+int FindMaxValue(int[] array)
+{
+    int max = array[0];
+    int index = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+            index = i;
+        }
+    }
+    int[] temp = new int[array.Length - 1];
+    for (int j = 0; j < index - 1; j++)
+    {
+        temp[j] = array[j];
+    }
+    for (int k = index; k < array.Length; k++)
+    {
+        temp[k] = array[k + 1];
+    }
+    array = temp;
+    return max;
+}
+
+Console.Write("Enter min of array: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter max of array: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] newArray = CreateArray(min, max, size);
+ShowArray(newArray);
+Console.WriteLine($"{FindMaxValue(newArray)}");
+Console.WriteLine($"{FindMaxValue(newArray)}");
---
+++ Homework 6/Program.cs
@@ -0,0 +1,80 @@
+﻿// CONST
+int[] CreateArray(int min, int max, int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+// ------------------------------------------------------
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+// void NumbersGreaterThan0(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] >= 0)
+//         {
+//             count++;
+//         }
+//     }
+//     Console.WriteLine($"Count of numbers greater than zero is {count}");
+// }
+
+// Console.Write("Enter min of array: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter max of array: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+
+// int[] newArray = CreateArray(min, max, size);
+// ShowArray(newArray);
+// Console.WriteLine();
+// NumbersGreaterThan0(newArray);
+// ----------------------------------------------------------------------------
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// void IntersectionPoint(int b1, int k1, int b2, int k2)
+// {
+//     int x = 0;
+//     int y = 0;
+//     if (k1 == k2)
+//     {
+//         Console.WriteLine("Straight lines are parallel");
+//     }
+//     else
+//     {
+//         x = (b2 - b1) / (k1 - k2);
+//         y = (k1 * x) + b1;
+//         Console.WriteLine($"Intersection point is ({x}, {y})");
+//     }
+// }
+
+// Console.Write("Enter b1: ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter k1: ");
+// int k1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter b2: ");
+// int b2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter k2: ");
+// int k2 = Convert.ToInt32(Console.ReadLine());
+
+// IntersectionPoint(b1, k1, b2, k2);
